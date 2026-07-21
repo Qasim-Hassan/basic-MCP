47014abb1d8fcd5a6217552a86ff4b99cf79af49
Qasim Hassan <qasimhassan1947@gmail.com>
2026-07-21 10:54:35 +0000

    working on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import './App.css'
 import { StytchLogin, IdentityProvider, useStytchUser } from '@stytch/react';
 
-
-
 function App() {
-
   const {user: User} = useStytchUser();
   const config = {
     products: [Products.oauth],
