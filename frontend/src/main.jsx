ad421d7afb853ddd1f6080a5b5a501efc8c9a17e
Qasim Hassan <qasimhassan1947@gmail.com>
2026-07-21 11:07:20 +0000

    fixing bugs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { StytchProvider } from '@stytch/react';
+import { createStytchUIClient } from '@stytch/react/ui';
+
 import './index.css'
 import App from './App.jsx'
-import { createStytchClient, StytchProvider } from '@stytch/react';
 
-const stytch = createStytchClient('public-token-test-55e71ffa-8f25-4632-8479-ca4a193b7464');
+const stytch = createStytchUIClient('public-token-test-55e71ffa-8f25-4632-8479-ca4a193b7464');
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StytchProvider stytch={stytch}>
-      <App />
-    </StytchProvider>
+      <StytchProvider stytch={stytch}>
+        <App />
+      </StytchProvider>
   </StrictMode>,
 )
